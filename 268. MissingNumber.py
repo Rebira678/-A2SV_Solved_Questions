7318ce00c242d4a0217effc78e72ebb9fe0f8d64
Rebira Adugna <rebikman9@gmail.com>
2026-02-04 18:12:42 +0300

    solution

---
+++ 268. MissingNumber.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/missing-number/description/
+#268. Missing Number
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)+1):
+            if i not in nums:
+                return i
