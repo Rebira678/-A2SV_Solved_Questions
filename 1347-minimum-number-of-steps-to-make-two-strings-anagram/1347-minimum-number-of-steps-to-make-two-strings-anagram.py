bbbf60e746de8390a34203081f5b7fb47d05f305
Rebira Adugna <rebikman9@gmail.com>
2026-02-11 09:46:19 +0300

    Time: 102 ms (61.44%), Space: 20 MB (6.96%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,29 +1,25 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
+        first=list(s)
+        second=list(t)
+
+        dic_first=defaultdict(int)
+        for i in first:
+            dic_first[i]+=1
         
-        s_counter = Counter(s)
-        t_counter = Counter(t)
+        dic_second=defaultdict(int)
+        for i in second:
+            dic_second[i]+=1
 
-      
-
-        count = 0
-
-        if s_counter == t_counter:
-            return 0
-
-        for key in s_counter:
-            while s_counter[key] != t_counter[key]:
-
-                if s_counter[key] > t_counter[key]:
-                    t_counter[key] += 1
-                    count += 2
-                    
-                elif s_counter[key] < t_counter[key]:
-                    t_counter[key] -= 1
-                    count -= 1
-
-        for ky in t_counter:
-            if ky not in s_counter:
-                count -= t_counter[ky]
-
+        count=0
+        for i in dic_first:
+            if i in dic_second:
+                m=dic_first[i]-dic_second[i]
+                if m>=0:
+                    count+=m
+            else:
+                count+=dic_first[i]
         return count
+
+
+        
