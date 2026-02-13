8efe844b070071fd38812394a9605bbf7bf638eb
Rebira Adugna <rebikman9@gmail.com>
2026-02-13 08:25:24 +0300

    Time: 0 ms (100%), Space: 19.5 MB (20.46%) - LeetHub

---
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumSwap(self, s1: str, s2: str) -> int:
+        xy = 0
+        yx = 0
+        
+        for i in range(len(s1)):
+            if s1[i] == 'x' and s2[i] == 'y':
+                xy += 1
+            elif s1[i] == 'y' and s2[i] == 'x':
+                yx += 1
+        
+        # if total mismatches is odd it become  impossible since it doesnot change anything
+        # instead it back another different
+        if (xy + yx) % 2 == 1:
+            return -1
+        
+        swaps = (xy // 2) + (yx // 2)
+        
+        # if one xy and one yx remain
+        if xy % 2 == 1:
+            swaps += 2
+        
+        return swaps
