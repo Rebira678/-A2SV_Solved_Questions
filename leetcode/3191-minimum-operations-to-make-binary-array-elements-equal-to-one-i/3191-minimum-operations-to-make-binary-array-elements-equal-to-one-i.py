3582a74dff621cc1feb5d7bec7ec07745ef08619
Rebira Adugna <rebikman9@gmail.com>
2026-03-27 18:43:49 +0300

    Add solution for Minimum Operations to Make Binary Array Elements Equal to One I

---
+++ leetcode/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        ans=0
+        def flip(nums,i):
+            if nums[i]:
+                nums[i]=0 
+            else:
+                nums[i]= 1
+        for i in range(len(nums)-2):
+            if nums[i]==0:
+                flip(nums,i)
+                flip(nums,i+1)
+                flip(nums,i+2)
+                ans+=1
+        if not nums[-1] or not nums[-2]:
+            return -1
+        
+        return ans
+
+
+        
