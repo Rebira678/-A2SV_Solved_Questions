dfba516e1c1e6e33fc91c2e0de75ca853eea9fb7
Rebira Adugna <rebikman9@gmail.com>
2026-04-01 20:14:05 +0300

    Time: 48 ms (30.49%), Space: 19.2 MB (48.59%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -7,13 +7,13 @@ class Solution:
         right=n
         ans=-1
 
-        while left<=right:
-            mid=(left+right)//2
+        while left <= right:
+            mid = (left + right)//2
+
             if isBadVersion(mid):
                 ans=mid
                 right=mid-1
             else:
                 left=mid+1
-
-        return ans
         
+        return ans
