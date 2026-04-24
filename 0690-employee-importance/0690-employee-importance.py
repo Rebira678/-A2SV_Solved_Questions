1551892728a93b36ea18fb35709ac7d79a8bc3ab
Rebira Adugna <rebikman9@gmail.com>
2026-04-24 18:48:53 +0300

    Time: 128 ms (55.04%), Space: 21.1 MB (18.7%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,31 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+
+        emp_map ={emp.id: emp for emp in employees}
+
+        def dfs(given_id):
+            emp=emp_map[given_id]
+            total = emp.importance
+            for sub_id in emp.subordinates:
+                total += dfs(sub_id)
+            return total
+        return dfs(id)
+
+
+
+
+
+
+
+        
+    
+       
